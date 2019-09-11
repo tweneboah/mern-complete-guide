6d1f289cf6082cd3d676eb1260fc064efbe0ceff
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-11 14:41:56 +0800

    Runing Server and Client at the same time

--- v2/app.js
+++ v2/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const url = process.env.PORT || 5000;
 
-app.listen(url, () => {
- console.log(`The server is runing on port ${url}`)
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+ console.log(`The server is runing on port ${PORT}`)
 })
