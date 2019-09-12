9008791a8863c5a3383026d4541ba06e3ed42c19
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-12 13:49:29 +0800

    Client side forms component created.

---
+++ v3-mern-crud/client/src/components/Todo-List.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>LIST OF TODOS</h2>
+            </div>
+        )
+    }
+}
