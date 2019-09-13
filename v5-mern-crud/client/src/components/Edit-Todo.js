0179deaeb03acc85019d5c0ea28b6621170bf688
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-13 08:00:56 +0800

    Creating Data and fetching data to backend using react

---
+++ v5-mern-crud/client/src/components/Edit-Todo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class EditTodo extends Component {
+    render() {
+        return (
+            <div>
+                <h2 on>EDIT todo</h2>
+                <form>
+
+                </form>
+            </div>
+        )
+    }
+}
